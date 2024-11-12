f9f0ff625f591322a71c95c80028c32f767b7a65
Mojo <do_not@email.me>
2024-11-12 18:06:03 +0600

    Added email validation, postman test case for email validation

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from typing import List
 
 app = FastAPI()
@@ -7,16 +7,18 @@ app = FastAPI()
 # In-memory storage (for simplicity)
 users_db = []
 
+
 class User(BaseModel):
     username: str
-    email: str
+    email: EmailStr
+
 
 @app.post("/users/")
 async def create_user(user: User):
     users_db.append(user)
     return {"message": "User created successfully", "user": user}
 
+
 @app.get("/users/", response_model=List[User])
 async def get_users():
     return users_db
-
